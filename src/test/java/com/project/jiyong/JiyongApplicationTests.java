1106359509ae19c1b48ed981df32a0e422e2f89b
jeffyun <jeffyun3061@gmail.com>
2024-02-21 14:46:29 +0900

    first commit

---
+++ src/test/java/com/project/jiyong/JiyongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.jiyong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiyongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
